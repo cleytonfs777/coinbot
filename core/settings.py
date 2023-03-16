6738a2b88b3d18fe7238ebb3c08a17c4d1c018e3
cleytonfs777 <cleyton.fs777@gmail.com>
2023-03-15 23:44:02 -0300

    correcoes

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('RAILWAY_DB_URL'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
